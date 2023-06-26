c112a6dde98a814fbefa64fdabc7de9f28f1e79b
nueces <nueces@gmail.com>
2023-06-26 03:41:47 +0200

    feat: ECR repository

---
+++ organization/01-ecr.tf
@@ -0,0 +1,58 @@
+
+module "ecr" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "1.6.0"
+
+  # We want to create one registry per environment to have a simple way to control who can upload images that are going
+  # to be deployed. Ideally for production only the CI would build and upload images to the registry.
+  for_each = {
+    for i in setproduct(var.projects, var.environments) : "${i[0]}-${i[1]}" => { project = i[0], env = i[1] }
+  }
+
+  repository_name = each.key
+
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+  create_lifecycle_policy           = true
+  # See:
+  #   - https://docs.aws.amazon.com/AmazonECR/latest/userguide/LifecyclePolicies.html#lifecycle_policy_parameters
+  #   - https://docs.aws.amazon.com/AmazonECR/latest/userguide/lifecycle_policy_examples.html#lp_example_difftype
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        "rulePriority" : 1,
+        "description" : "Remove tagged images if there are more than ${lookup(var.ecr_keep_images_count, each.value.env)}",
+        "selection" : {
+          "tagStatus" : "tagged",
+          "tagPrefixList" : [each.value.env],
+          "countType" : "imageCountMoreThan",
+          "countNumber" : lookup(var.ecr_keep_images_count, each.value.env)
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      },
+      {
+        "rulePriority" : 2,
+        "description" : "Remove untagged images after ${lookup(var.ecr_keep_untagged_images_days, each.value.env)} day",
+        "selection" : {
+          "tagStatus" : "untagged",
+          "countType" : "sinceImagePushed",
+          "countUnit" : "days",
+          "countNumber" : lookup(var.ecr_keep_untagged_images_days, each.value.env)
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      },
+    ]
+  })
+
+  repository_force_delete = true
+
+  tags = merge(var.default_tags, {
+    Name        = each.key
+    Project     = each.value.project
+    Environment = each.value.env
+    }
+  )
+}
---
+++ organization/variables.tf
@@ -0,0 +1,83 @@
+#############################################################################
+# Variables
+#############################################################################
+
+# A simple list of projects to express the idea.
+variable "projects" {
+  description = "List of projects in the organization"
+  type        = list(string)
+  default     = ["qcase"]
+}
+
+variable "environments" {
+  description = "Environments name"
+  type        = list(string)
+  default     = ["dev"]
+
+  validation {
+    condition     = alltrue([for env in var.environments : contains(["dev", "stg", "prd"], env)])
+    error_message = "The environments name values must be one or more of the following ones \"dev\", \"stg\", or \"prd\"."
+  }
+}
+
+# TODO: Remove default value and pass this value via config options or a separate tfvars file.
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+  # Select the desired regions to be used.
+  # only us, non gov: can(regex("^us(-gov)?-(east|west)-[1-9]$", var.aws_region))
+  # only us, non gov: can(regex("^us-(east|west)-[1-9]$", var.aws_region))
+  # only us-gov:      can(regex("^us-gov-(east|west)-[1-9]$", var.aws_region))
+  # only eu:          can(regex("^eu-(central|north|south|west)-[1-9]$", var.aws_region))
+  # only ap:          can(regex("^ap-(east|northeast|south|southeast)-[1-9]$", var.aws_region))
+  # ANY: can(regex("^(us(-gov)?|ap|ca|cn|eu|sa)-(central|(north|south)?(east|west)?)-[1-9]$", var.aws_region))
+  validation {
+    # Allow only us and eu regions
+    condition = anytrue([
+      can(regex("^us-(east|west)-[1-9]$", var.aws_region)),
+      can(regex("^eu-(central|north|south|west)-[1-9]$", var.aws_region)),
+    ])
+    error_message = "The aws select is invalid or is not allowed to be used on this project."
+  }
+}
+
+variable "availability_zones" {
+  type        = any # null or list(string)
+  description = "Availability Zones. If null terraform would select N `availability_zones_amount` AZ from the region."
+  default     = null
+}
+
+variable "default_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default = {
+    Environment = "ToBeSet"
+    Owner       = "Org"
+    Terraform   = "true"
+  }
+}
+
+#############################################################################
+# ECR
+#############################################################################
+
+variable "ecr_keep_images_count" {
+  description = "Total number of images to keep in the ECR repository. Defined per environment."
+  type        = map(number)
+  default = {
+    dev = 3
+    stg = 3
+    prd = 6
+  }
+}
+
+variable "ecr_keep_untagged_images_days" {
+  description = "Number of days that untagged images are keep in the registry. Defined per environment."
+  type        = map(number)
+  default = {
+    dev = 1
+    stg = 1
+    prd = 3
+  }
+}
