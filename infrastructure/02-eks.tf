94a44a2071fc32edb6b4befc9968a205e9f6b123
nueces <nueces@gmail.com>
2023-06-27 04:00:24 +0200

    feat: Improve EKS definition

--- infrastructure/02-eks.tf
+++ infrastructure/02-eks.tf
@@ -4,31 +4,39 @@ module "eks" {
   version = "19.15.3"
 
   cluster_name    = local.configurations.project_name
-  cluster_version = "1.27"
+  cluster_version = var.eks_cluster_version
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+    ami_type       = var.eks_managed_node_group_defaults.ami_type
+    instance_types = var.eks_managed_node_group_defaults.instance_types
+    key_name       = coalesce(var.eks_key_name, local.configurations.key_name)
 
+    min_size     = var.eks_managed_node_group_defaults.min_size
+    max_size     = var.eks_managed_node_group_defaults.max_size # <= min * (100/max_unavailable_percentage) ?
+    desired_size = var.eks_managed_node_group_defaults.desired_size
   }
 
+  # TODO:
+  #   replace with a dynamic block
+  #   move default values to the previous section.
   eks_managed_node_groups = {
-    web_applications = {
-      name        = "qweb"
-      description = "EKS managed node group for web applications"
+    apps = {
+      name        = var.eks_managed_node_groups.name
+      description = var.eks_managed_node_groups.description
 
-      instance_types = ["t3.micro", "t3.small"]
-      capacity_type  = "SPOT"
+      capacity_type  = var.eks_managed_node_groups.capacity_type
+      instance_types = var.eks_managed_node_groups.instance_types
 
-      min_size     = 1 # 3
-      max_size     = 6 # <= min * (100/max_unavailable_percentage)
-      desired_size = 1 # 3 one per az
+      min_size     = var.eks_managed_node_groups.min_size
+      max_size     = var.eks_managed_node_groups.max_size # <= min * (100/max_unavailable_percentage) ?
+      desired_size = var.eks_managed_node_groups.desired_size
 
       update_config = {
-        max_unavailable_percentage = 50
+        max_unavailable_percentage = var.eks_managed_node_groups.max_unavailable_percentage
       }
     }
   }
