9d300a07e7f1ef401b92678923b2e111e80ab574
nueces <nueces@gmail.com>
2023-06-27 17:18:34 +0200

    feat: Add AWS load balancer controller

--- infrastructure/03-helm.tf
+++ infrastructure/03-helm.tf
@@ -1,5 +1,5 @@
 #############################################################################
-## Applications deployment using helm
+## Helm deployments
 ##
 
 locals {
@@ -7,6 +7,54 @@ locals {
   commit_tag    = one([for tag in data.aws_ecr_image.qweb.image_tags : tag if can(regex("^[a-z0-9]{8}$", tag))])
 
 }
+
+#############################################################################
+##
+## Load balancer
+
+resource "helm_release" "load-balancer" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  #  depends_on = [
+  #    kubernetes_service_account.aws-load-balancer-controller
+  #  ]
+
+  set {
+    name  = "region"
+    value = data.aws_region.current.name
+  }
+
+  set {
+    name  = "vpcId"
+    value = module.vpc.vpc_id
+  }
+
+  set {
+    name  = "image.repository"
+    value = "602401143452.dkr.ecr.eu-west-2.amazonaws.com/amazon/aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = kubernetes_service_account.aws-load-balancer-controller.metadata.0.name
+  }
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+}
+
+#############################################################################
+##
+## Applications
 
 data "aws_ecr_repository" "qweb" {
   name = "${local.project_name}/qweb-${var.environment}"
