5d03a84c6207138c86989462298e61b0b09700e2
nueces <nueces@gmail.com>
2023-06-27 17:18:34 +0200

    feat: Add AWS load balancer controller

--- infrastructure/02-eks.tf
+++ infrastructure/02-eks.tf
@@ -46,3 +46,37 @@ module "eks" {
 
   tags = var.default_tags
 }
+
+#############################################################################
+## Load Balancer
+##
+
+module "load-balancer-role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                              = "${local.project_name}_eks_lb"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+
+resource "kubernetes_service_account" "aws-load-balancer-controller" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+      "app.kubernetes.io/component" = "controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn"               = module.load-balancer-role.iam_role_arn
+      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+    }
+  }
+}
